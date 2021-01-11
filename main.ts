3e49e87930211df15af6cd10825e805d20dd4318
cakepower <32028127+cakepower@users.noreply.github.com>
2021-01-11 22:50:58 +0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,16 @@
 input.onButtonPressed(Button.A, function () {
-    music.playMelody("C D C D E E E G ", 120)
     strip.showColor(neopixel.colors(NeoPixelColors.Indigo))
+    strip.show()
     basic.pause(100)
+    music.playMelody("C D C D E E E G ", 120)
     strip.showColor(neopixel.colors(NeoPixelColors.Black))
 })
 input.onButtonPressed(Button.B, function () {
-    music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
     strip.showColor(neopixel.colors(NeoPixelColors.Orange))
+    strip.show()
     basic.pause(100)
+    music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
+    basic.pause(200)
     strip.showColor(neopixel.colors(NeoPixelColors.Black))
 })
 let strip: neopixel.Strip = null
